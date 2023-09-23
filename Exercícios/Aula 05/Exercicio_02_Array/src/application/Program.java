6bad9bfabb12952b4bf1bc56ca368984a2b39c93
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-09-22 22:05:27 -0300

    Exercicio 02 sobre Array aula 05

---
+++ Exercícios/Aula 05/Exercicio_02_Array/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        List<Integer> numerosSortidos = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int numerosSortido = random.nextInt(100) + 1;
+            numerosSortidos.add(numerosSortido);
+        }
+
+        System.out.print("Digite um número: ");
+        int numeroInserido = sc.nextInt();
+
+        boolean encontrado = false;
+
+        for (int numero : numerosSortidos) {
+            if (numero == numeroInserido) {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("O número " + numeroInserido + " foi encontrado");
+        } else {
+            System.out.println("O número " + numeroInserido + " não foi encontrado");
+        }
+    }
+}
