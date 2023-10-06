4e3f02f54996eaef208613a58951b1500b27fcd4
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-10-06 18:13:35 -0300

    feat: exercicio 01 resolvido

--- Exercícios/Aula 08/Exercicio_01_Maven_Stream/src/main/java/caldeira/java/Program.java
+++ Exercícios/Aula 08/Exercicio_01_Maven_Stream/src/main/java/caldeira/java/Program.java
@@ -1,18 +1,33 @@
 package caldeira.java;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
 
-        List<String> listaNomes =  Arrays.asList("Iago", "Pedro","Larissa", "João");
+        List<String> listaNomes = Arrays.asList("Iago", "Larissa", "João", "piaaaaaaaaaaaaaaaaaaaaaa", "Larissa", "Iago", "Iago", "Iago");
 
         long count = listaNomes.stream().count();
         System.out.println("Tamanho da lista " + count);
 
-        String maiorNome = listaNomes.stream().findFirst().get();
+        /*.max(Comparator.comparingInt(String::length)): Nesta parte, você está usando a operação max do stream
+             para encontrar o elemento máximo na sequência. No entanto, você precisa fornecer um comparador que
+             determine como os elementos devem ser comparados para determinar o máximo.
+             O Comparator.comparingInt(String::length) faz isso. Ele compara os elementos da sequência (nomes) com
+             base em seu comprimento (número de caracteres), e isso é feito usando a referência de método String::length. */
+        String maiorNome = listaNomes.stream().max(Comparator.comparingInt(String::length)).orElse("");
         System.out.println("O maior nome é: " + maiorNome);
 
+        Map<String, Long> contagemStrings = listaNomes.stream()
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+
+        contagemStrings.entrySet().stream()
+                .sorted((entry1, entry2) -> Long.compare(entry2.getValue(), entry1.getValue()))
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
 }
+
