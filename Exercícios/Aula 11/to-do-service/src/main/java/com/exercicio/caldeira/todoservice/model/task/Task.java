41b5305fc1274594ca3a01357ee5c23891465199
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-10-28 20:28:59 -0300

    Criado classe Model

--- Exercícios/Aula 11/to-do-service/src/main/java/com/exercicio/caldeira/todoservice/model/task/Task.java
+++ Exercícios/Aula 11/to-do-service/src/main/java/com/exercicio/caldeira/todoservice/model/task/Task.java
@@ -1,21 +1,19 @@
-package com.exercicio.caldeira.todoservice.model.task.task;
+package com.exercicio.caldeira.todoservice.model.task;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
-
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Task {
     private Long id;
     private String descricao;
     private LocalDate dataDeVencimento;
     private boolean estado;
 }
+
+
+
