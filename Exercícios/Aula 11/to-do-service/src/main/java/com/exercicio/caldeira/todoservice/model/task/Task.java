ac0f3cd945edd86f89d2f0fc9100fe8e232bfa35
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-11-06 14:32:58 -0300

    Adicionado anotaçÃões de validaçÃão

--- Exercícios/Aula 11/to-do-service/src/main/java/com/exercicio/caldeira/todoservice/model/task/Task.java
+++ Exercícios/Aula 11/to-do-service/src/main/java/com/exercicio/caldeira/todoservice/model/task/Task.java
@@ -1,17 +1,22 @@
 package com.exercicio.caldeira.todoservice.model.task;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import lombok.*;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class Task {
     private Long id;
+    @NotBlank(message = "A tarfea não pode estar vazia")
     private String descricao;
+    @Future(message = "A data de vencimento deve estar no futuro!")
+    @NotNull(message = "A data não estar vazia!")
     private LocalDate dataDeVencimento;
+    @NotNull
     private boolean estado;
 }
 
