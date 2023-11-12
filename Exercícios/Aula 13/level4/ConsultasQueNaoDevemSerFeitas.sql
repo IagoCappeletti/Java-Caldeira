23a0c4e25e4fa66e5bdb3cae8b8114eba3e498e3
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-11-12 13:57:53 -0300

    Aprendendo o que não fazer no banco de dados

---
+++ Exercícios/Aula 13/level4/ConsultasQueNaoDevemSerFeitas.sql
@@ -0,0 +1,7 @@
+//Fazendo  um update sem o where.
+UPDATE Clientes SET nome = 'TESTE'
+
+//Dropando a tabela funcionario e tentando exibir ela depois.
+DROP TABLE funcionario;
+
+SELECT * FROM funcionario;
